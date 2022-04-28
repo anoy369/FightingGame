e50bd37d96eb87da517b3160690cfdf9bd19670f
anoy369 <adannoy369@gmail.com>
2022-04-29 03:43:25 +0600

    add player movement left, right and jump

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ class Sprite {
     update() {
         this.draw()
         
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -52,12 +53,65 @@ const enemy = new Sprite({
     }
 })
 
+let lastKey
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+    
+// Player movement
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+        break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+        break
+        case 'w':
+            player.velocity.y = -10
+        break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed = false
+        break
+        case 'a':
+            keys.a.pressed = false
+        break
+        case 'w':
+            keys.w.pressed = false
+        break
+    }
+})
