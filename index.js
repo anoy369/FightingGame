11752d792abfc1e7c6e8b3ba867c4010b9059d57
anoy369 <adannoy369@gmail.com>
2022-04-29 19:39:31 +0600

    add player and enemy attack detection

--- index.js
+++ index.js
@@ -8,20 +8,45 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'red', offset }) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.width = 50
         this.lastKey
+        this.color = color
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50
+        }
+        this.isAttacking
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // attack box
+        if (this.isAttacking){
+            c.fillStyle = 'green'
+            c.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.width,
+                this.attackBox.height
+            )
+        }        
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x
+        this.attackBox.position.y = this.position.y
         
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -29,6 +54,13 @@ class Sprite {
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
         } else this.velocity.y += gravity
+    }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking - false
+        }, 100)
     }
 }
 
@@ -40,6 +72,10 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 10
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 })
 
@@ -50,6 +86,11 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
+        y: 0
+    },
+    color: 'blue',
+    offset: {
+        x: -50,
         y: 0
     }
 })
@@ -67,6 +108,15 @@ const keys = {
     ArrowLeft: {
         pressed: false
     }
+}
+
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
 }
 
 function animate() {
@@ -91,6 +141,30 @@ function animate() {
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
     }
+
+    //detect for collusion for player
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false
+        console.log('attack')
+    }
+
+    //detect for collusion for enemy
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false
+        console.log('Eattack')
+    }
 }
 
 animate()
@@ -109,6 +183,9 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -20
         break
+        case ' ':
+            player.attack()
+        break
 
         // enemy keys
         case 'ArrowRight':
@@ -122,8 +199,10 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowUp':
             enemy.velocity.y = -20
         break
+        case 'ArrowDown':
+            enemy.isAttacking = true
+        break
     }
-    console.log(event.key)
 })
 
 window.addEventListener('keyup', (event) => {
